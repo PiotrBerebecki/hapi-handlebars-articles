8caa3fba1abc0b5b787e7481248c0a6e17d5ea61
PiotrBerebecki <petebere@gmail.com>
2017-04-08 15:27:41 +0100

    Display articles

--- src/database/get.js
+++ src/database/get.js
@@ -7,11 +7,17 @@ const get = {};
 get.articles = (callback) => {
 
   const sqlQuery = `
-    SELECT * FROM articles
+    SELECT u.first_name || ' ' || u.last_name AS author_display_name, u.avatar_url as author_avatar_url,
+           a.title, a.body, a.date_posted, a.image_url
+      FROM articles AS a
+           INNER JOIN users AS u
+           ON u.id = a.author_id
+    ORDER BY a.id DESC
   `;
 
   connect.query(sqlQuery, (err, response) => {
     if (err) { return callback(err); }
+    console.log('===== response.rows', response.rows);
     callback(null, response.rows);
   });
 
--- test/handlebars.js
+++ test/handlebars.js
@@ -4,12 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
 
-// register helper
+// register handlebars helper
 require('./../src/views/helpers/format-date');
 
 
-tape('hanbdlebars formatDate helper', (t) => {
-
+tape('handlebars formatDate helper', (t) => {
   const partial = fs.readFileSync(path.join(__dirname, './../src/views/partials/main.hbs'), 'utf8');
   const template = handlebars.compile(partial);
 
@@ -20,9 +19,28 @@ tape('hanbdlebars formatDate helper', (t) => {
   };
 
   const html = template(context);
-  t.ok(html.indexOf('Jan 1, 1970') !== -1, 'should return formatted date');
+  t.ok(html.indexOf('Jan 1, 1970') !== -1, 'should render formatted date');
   t.end();
 });
+
+
+tape('handlebars render display name of authors of all articles', (t) => {
+  const partial = fs.readFileSync(path.join(__dirname, './../src/views/partials/main.hbs'), 'utf8');
+  const template = handlebars.compile(partial);
+
+  const context = {
+    articles: [
+      {author_display_name: 'Grey Kitty'},
+      {author_display_name: 'Orange Cat'},
+    ]
+  };
+
+  const html = template(context);
+  t.ok(html.indexOf('Grey Kitty') !== -1, `should render ${context.articles[0].author_display_name}`);
+  t.ok(html.indexOf('Orange Cat') !== -1, `should render ${context.articles[1].author_display_name}`);
+  t.end();
+});
+
 
 
 
